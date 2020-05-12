99faf2852674d44a24c8f417ce6c44c7bc4e8dba
Thanh An <annt@ows.vn>
2020-05-12 10:40:22 +0700

    first init

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "grpc-test/interface"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	host = "localhost"
+	port = "8888"
+)
+
+func main() {
+
+	conn, err := grpc.Dial(host+":"+port, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("cannot connect: %v", err)
+	}
+	defer conn.Close()
+
+	myClient := pb.NewUserServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	req := pb.AuthInfo{
+		Email:    "annt@ows.vn",
+		Password: "12345678",
+	}
+
+	res, err := myClient.CreateUser(ctx, &req)
+	if err != nil {
+		log.Fatalf("could not GetLessonInfo: %v", err)
+	}
+
+	log.Printf("GetLessonInfo: %v", res)
+
+}
